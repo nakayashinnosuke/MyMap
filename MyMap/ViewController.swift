b129c79d6435a2522ea2de02a1abd16d5160a104
Shinnosuke <kuchikarademakase1208@gmail.com>
2019-04-16 10:18:42 +0900

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by macuser on 2019/04/16.
+//  Copyright © 2019 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
